4719e44f20f139327005ff082c95362dd78f7ce6
Fernando Garibay <garibaycejaf@gmail.com>
2022-03-08 13:38:09 -0600

    + Clases: Elefante, Pato, Leon, Animal

--- Estructura de clases/Estructura de clases.cpp
+++ Estructura de clases/Estructura de clases.cpp
@@ -5,6 +5,101 @@
 
 using namespace std;
 
+class Elefante {
+public:
+	Elefante();
+	~Elefante();
+	void dibuja();
+	void imprime();
+};
+
+Elefante::Elefante() {
+
+}
+
+Elefante::~Elefante() {}
+
+void Elefante::dibuja() {
+
+}
+
+void Elefante::imprime() {
+
+}
+
+
+class Pato {
+public:
+	Pato();
+	~Pato();
+	void dibuja();
+	void imprime();
+};
+
+Pato::Pato() {
+
+}
+
+Pato::~Pato() {}
+
+void Pato::dibuja() {
+
+}
+
+void Pato::imprime() {
+
+}
+
+
+class Leon {
+public:
+	Leon();
+	~Leon();
+	void dibuja();
+	void imprime();
+};
+
+Leon::Leon() {
+
+}
+
+Leon::~Leon() {}
+
+void Leon::dibuja() {
+
+}
+
+void Leon::imprime() {
+
+}
+
+class Animal {
+private:
+	Elefante objElefante;
+	Pato objPato;
+	Leon objLeon;
+public:
+	Animal();
+	~Animal();
+	void dibuja();
+	void imprime();
+};
+
+Animal::Animal() {
+	
+}
+
+Animal::~Animal() {}
+
+void Animal::dibuja() {
+
+}
+
+void Animal::imprime() {
+
+}
+
+
 int main(void) {
 
 	return 0;
