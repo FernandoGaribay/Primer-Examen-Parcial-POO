9eda8513d980c38efb712d5303fef10165f115d1
Fernando Garibay <garibaycejaf@gmail.com>
2022-03-08 13:22:20 -0600

    Agregar archivos de proyecto.

---
+++ Estructura de clases/Estructura de clases.cpp
@@ -0,0 +1,11 @@
+// Escribe el código necesario para crear la estructura de clases, solo es necesario declarar las clases, y en el main, 
+// crear una instancia, no se requieren datos
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	return 0;
+}
