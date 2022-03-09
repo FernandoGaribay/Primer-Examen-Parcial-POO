9272e1f5be0ec85709b0188a4513c311e7e117ed
Fernando Garibay <96392505+FernandoGaribay@users.noreply.github.com>
2022-03-08 18:02:32 -0600

    +Clase "Perro"

--- Clase Perro/Clase Perro.cpp
+++ Clase Perro/Clase Perro.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#pragma warning (disable:4996)
 
 using namespace std;
 
-int main(void)
+class Perro {
+private:
+	char const* nombre;
+public:
+	Perro();
+	~Perro();
+	void ladrar();
+	void setNombre(char const*);
+	char const* getNombre();
+};
+
+Perro::Perro() {
+
+}
+
+Perro::~Perro() {}
+
+void Perro::ladrar() {
+	cout << nombre <<": Guau Guau!" << endl;
+}
+
+void Perro::setNombre(char const* nombre_) {
+	nombre = nombre_;
+}
+
+char const* Perro::getNombre() {
+	return nombre;
+}
+
+
+int main()
 {
-    return 0;
+	Perro manchas = Perro();
+
+	manchas.setNombre("Manchas");
+	manchas.ladrar();
+
+	return 0;
 }
